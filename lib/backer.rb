1512286e5d16f5f34188ab6c5f41467af32bc820
Sushi Chanrai <sushi@pathworks.co>
2018-07-19 20:40:07 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,8 +6,9 @@ class Backer
     @backed_projects = []
   end
 
-  def back_project(project)
-    @backed_projects << project
+  def back_project(project_a)
+    project.backer = self
+    @backed_projects << project_a
   end
 
   def self.all
